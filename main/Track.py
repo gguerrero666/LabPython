a1508b5fdbd1ed8fbd93c8f401778bb7e12e988a
gguerrero <guillermo_guerrero@outlook.com>
2023-09-08 18:13:45 -0600

    youtube quality

--- main/Track.py
+++ main/Track.py
@@ -1,6 +1,9 @@
 from ultralytics import YOLO
 
+ytQuality = "?vq=small"
+yt = "https://youtu.be/8DpjANtV-bs" + ytQuality
+
 # Configure the tracking parameters and run the tracker
 model = YOLO("../yolo/yolov8n.pt")
-results = model.track(source="https://youtu.be/8DpjANtV-bs", conf=0.3, iou=0.5, show=True,
-                      imgsz=640, verbose=True)
+results = model.track(source=yt, conf=0.3, iou=0.5, show=True,
+                      imgsz=640, verbose=False)
--- main/Youtube.py
+++ main/Youtube.py
@@ -1,4 +1,7 @@
 from ultralytics import YOLO
 
+ytQuality = "?vq=small"
+yt = "https://youtu.be/8DpjANtV-bs" + ytQuality
+
 model = YOLO("yolo/yolov8n.pt")
-results = model.track(source="https://youtu.be/8DpjANtV-bs", conf=0.3, iou=0.5, show=True, verbose=False)
+results = model.track(source=yt, conf=0.3, iou=0.5, show=True, verbose=False)
